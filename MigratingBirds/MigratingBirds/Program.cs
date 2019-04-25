f9b4030dc1a11e1809c13966c715a2f8ba684670
STEPSTONE-ASA\mchugg01 <mchugg01@stepstone.com>
2019-04-25 12:39:34 +0100

    adding migrating birds solution

---
+++ MigratingBirds/MigratingBirds/Program.cs
@@ -0,0 +1,32 @@
+﻿//link to problem:
+//https://www.hackerrank.com/challenges/migratory-birds/problem
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MigratingBirds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int arrCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            int result = migratoryBirds(arr);
+
+            Console.WriteLine(result);
+        }
+
+        static int migratoryBirds(List<int> arr)
+        {
+            var most = arr.GroupBy(i => i).OrderByDescending(grp => grp.Count())
+                .Select(grp => grp.Key).First();
+
+            return most;
+        }
+    }
+}
